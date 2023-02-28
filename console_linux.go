398722e701783ba57375009a58639211fb4392a8
jlortiz0 <jlortiz0@users.noreply.github.com>
2023-02-27 16:30:49 -0800

    Fixed linux build

--- console_linux.go
+++ console_linux.go
@@ -1,3 +1,5 @@
 package main
 
 func hideConsole() {}
+
+func highlightFile(_, _ string) {}
--- console_windows.go
+++ console_windows.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+
 	"github.com/TheTitanrain/w32"
 	"golang.org/x/sys/windows"
 )
@@ -13,4 +18,12 @@ func hideConsole() {
 			w32.ShowWindow(console, w32.SW_HIDE)
 		}
 	}
+}
+
+func highlightFile(p1, p2 string) {
+	cwd, _ := os.Getwd()
+	cmd := exec.Command("explorer", "/select,", fmt.Sprintf("\"%s%c%s%c%s\"", cwd, os.PathSeparator, p1, os.PathSeparator, p2))
+	cwd = fmt.Sprintf("explorer /select,%s", cmd.Args[2])
+	cmd.SysProcAttr = &syscall.SysProcAttr{CmdLine: cwd}
+	cmd.Run()
 }
