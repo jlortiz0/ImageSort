16ab321e64e17c622dc100a8fb86fec232f9a7b9
jlortiz0 <jlortiz0@users.noreply.github.com>
2024-12-30 21:41:55 -0800

    Don't copy Hashes in benchmark

--- bitbench/bench.go
+++ bitbench/bench.go
@@ -3,6 +3,7 @@ package bitbench
 // #include "libpopcnt.h"
 // #include "bitcnt.h"
 import "C"
+import "math/bits"
 
 const HASH_SIZE = 0x0C
 const HASH_DIFF = 10
@@ -14,7 +15,7 @@ var bitsTable [16]uint16 = [16]uint16{
 	1, 2, 2, 3, 2, 3, 3, 4,
 }
 
-func TableDiff(h1 Hash, h2 Hash) bool {
+func TableDiff(h1 *Hash, h2 *Hash) bool {
 	var c uint16
 	for k := 0; k < len(h1); k++ {
 		temp := h1[k] ^ h2[k]
@@ -27,22 +28,29 @@ func TableDiff(h1 Hash, h2 Hash) bool {
 	return c <= HASH_DIFF
 }
 
-func XorAll(h1 Hash, h2 Hash) bool {
-	return int(C.xor_all((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), C.ulonglong(len(h1)))) <= HASH_DIFF
+func TableDiff2(h1 *Hash, h2 *Hash) bool {
+	var c int
+	for k := 0; k < len(h1); k++ {
+		c += bits.OnesCount8(h1[k] ^ h2[k])
+		if c > HASH_DIFF {
+			break
+		}
+	}
+	return c <= HASH_DIFF
 }
 
-func XorIncr(h1 Hash, h2 Hash) bool {
-	return bool(C.blob_similar((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), C.ulonglong(len(h1))))
+func XorAll(h1 *Hash, h2 *Hash) bool {
+	return int(C.xor_all((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), HASH_SIZE)) <= HASH_DIFF
 }
 
-func XorIncr2(h1 Hash, h2 Hash) bool {
-	return bool(C.blob_similar_alt((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), C.ulonglong(len(h1))))
+func XorIncr(h1 *Hash, h2 *Hash) bool {
+	return bool(C.blob_similar((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), HASH_SIZE))
 }
 
-func XorIncr3(h1 Hash, h2 Hash) bool {
-	return bool(C.blob_similar_alt2((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), C.ulonglong(len(h1))))
+func XorIncr2(h1 *Hash, h2 *Hash) bool {
+	return bool(C.blob_similar_alt((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), HASH_SIZE))
 }
 
-func main() {
-
+func XorIncr3(h1 *Hash, h2 *Hash) bool {
+	return bool(C.blob_similar_alt2((*C.uchar)(&h1[0]), (*C.uchar)(&h2[0]), HASH_SIZE))
 }
