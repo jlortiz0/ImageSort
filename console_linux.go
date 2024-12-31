c2ec094d08a099f96c804fc09e8e1a9cba8b15c0
jlortiz0 <jlortiz0@users.noreply.github.com>
2024-12-31 14:06:39 -0800

    xdg-open attempt

--- console_linux.go
+++ console_linux.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
 func hideConsole() {}
 
-func highlightFile(_, _ string) {}
+func highlightFile(p1, _ string) {
+	cwd, _ := os.Getwd()
+	// xdg-open can block depending on what program is launched, so wait in a goroutine.
+	go exec.Command("xdg-open", filepath.Join(cwd, p1)).Run()
+}
 
-func viewFile(_ string) {}
+func viewFile(p string) {
+	go exec.Command("xdg-open", p).Run()
+}
